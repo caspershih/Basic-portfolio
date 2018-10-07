3f9f4f626a1c6dc68fbc38038f8e77c58ab23426
Casper Shih <casshih@gmail.com>
2018-10-07 13:58:38 -0700

    adding function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
-const nodemailer 
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+//This app is running on express server
+const app = express();
+
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => console.log("Server started"));
